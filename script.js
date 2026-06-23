6b2c04ac27fe997300c246838cdcd814b29eb1f8
Rogerio Luiz <rogeriovnuzpizolato@gmail.com>
2026-06-23 13:31:40 -0300

    feita a logica para adicionar os habitos ao dom

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const dataHoje= document.getElementById("data-hoje");
+const dateToday= document.getElementById("data-hoje");
+const inputHabit= document.getElementById("input-habito");
+const btnAdd= document.getElementById("btn-add");
+const listHabit= document.getElementById("lista-habitos");
+
+let arrHabits= []
 
 function mostrarHora(){
     const data= new Date();
@@ -10,8 +15,49 @@ function mostrarHora(){
     };
     const dataCompleta= data.toLocaleDateString("pt-br", dataFormada);
 
-    dataHoje.innerHTML= dataCompleta;
+    dateToday.innerHTML= dataCompleta;
 
-    setInterval(mostrarHora,1000);
+    
 }
+
+function criaHabito(){
+    const habits= {
+        desc: inputHabit.value,
+        id: Date.now(),
+        finish: false
+    };
+
+    arrHabits.push(habits);
+
+
+    listHabit.innerHTML= arrHabits.map(habit=> `
+        <li class="habito-card">
+         <input 
+         type="checkbox"
+         class="habito-check"
+         >
+         <span class="habito-nome">${habits.desc}</span>
+         <button class="btn-excluir">✕</button>
+        </li>`
+    )
+    .join("");
+
+    let li= document.querySelector(".habito-card")
+
+    const inputCheck= document.querySelectorAll(".habito-check");
+    inputCheck.forEach(check=>{
+        check.addEventListener("change", ()=>{
+            li= check.closest(".habito-card");
+
+            if(check.checked){
+                li.classList.add("concluido")
+            }else{
+                li.classList.remove("concluido")
+            }
+        });
+    });
+}
+
+btnAdd.addEventListener("click", criaHabito)
+
 mostrarHora()
